432dd44cf872f1ece2199c17272f6f33da562275
Eric Tendian <erictendian@gmail.com>
2016-05-28 19:57:57 -0500

    Refactoring script to use piping, reducing number of temp files

--- getlastminute.sh
+++ getlastminute.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 ORIGINAL=`ls -t *.mp3 | head -n 1` # the file which was modified most recently (most recent ffmpeg segment)
-NOSILENCE="output/lastminute-nosilence.mp3" # the name of our temporary file
-OUTPUT="output/lastminute.mp3" # the name of our output file
+OUTPUT="output/lastminute.mp3" # the name of our output file (@TODO: needs to be unique)
 
-# the user doesn't want much silence in their recording
-if [ "$1" == "nosilence" ]; then
-    # let's get the last 10 minutes (600s) of the stream so we don't work with a very large file
-    ffmpeg -sseof -600 -t 600 -i $ORIGINAL "output/last10min.mp3"
-    # remove the silence from the last 10 minutes of audio
-    # silence longer than 1s will get truncated to 0.5s
-    # "silence" is defined as being over the 1.07% threshold
-    # (arrived at this number thru trial-and-error working with the Zone 10 stream)
-    sox "output/last10min.mp3" $NOSILENCE silence -l 1 0.5 1.07% -1 1.0 1.07%
-    rm "output/last10min.mp3" # remove our temporary file
-    # pull the last 30 seconds from our silence-removed file, not the original mp3
-    ORIGINAL=$NOSILENCE
-fi
+# let's get the last 10 minutes (600s) of the stream so we don't work with a very large file
+ffmpeg -sseof -600 -t 600 -i $ORIGINAL -f mp3 - |
+
+# check if the user doesn't want much silence in their recording (otherwise just save the output)
+( [[ "$1" == "nosilence" ]] && sox -t mp3 - -t mp3 $OUTPUT.tmp silence -l 1 0.5 1.07% -1 0.5 1.07% || cat > $OUTPUT.tmp )
+# remove the silence from the last 10 minutes of audio
+# silence longer than 0.5s will get truncated to 0.5s
+# "silence" is defined as being over the 1.07% threshold
+# (arrived at this number thru trial-and-error working with the Zone 10 stream)
 
 # get the last 60 seconds of the mp3, and write it to our output file
-ffmpeg -sseof -60 -t 60 -i $ORIGINAL -c copy $OUTPUT -y
-rm $NOSILENCE
+ffmpeg -sseof -60 -t 60 -i $OUTPUT.tmp -c copy $OUTPUT -y
+# this cannot be done with pipes because we need to use sseof
+
+# remove the temporary file
+rm $OUTPUT.tmp
 
 # play the generated file
 mpg123 $OUTPUT
+
